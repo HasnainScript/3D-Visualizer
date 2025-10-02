7eba81ee3ca08ad77b1e69ec54b1c8e9b193237a
HasnainScript <hasnainhaider.developer@gmail.com>
2025-10-01 21:38:40 -0600

    Removed unused files

--- src/GeometryEngine.cpp
+++
@@ -1,6 +0,0 @@
-#include "GeometryEngine.h"
-#include <BRepPrimAPI_MakeBox.hxx>
-
-TopoDS_Shape GeometryEngine::createBox(double dx, double dy, double dz) {
-    return BRepPrimAPI_MakeBox(dx, dy, dz).Shape();
-}

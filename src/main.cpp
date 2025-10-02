5641a4e6d694014d0aab1ca41a567a3d1500babe
HasnainScript <hasnainhaider.developer@gmail.com>
2025-10-01 22:11:23 -0600

    Added Lights

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,73 @@
 #include <vtkActor.h>
-#include <vtkCamera.h>
 #include <vtkConeSource.h>
-#include <vtkNamedColors.h>
-#include <vtkNew.h>
+#include <vtkSphereSource.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkProperty.h>
-#include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkNew.h>
 #include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkLight.h>
 
 int main()
 {
- 
-
+    // --- First object: Cone ---
     vtkNew<vtkConeSource> cone;
     cone->SetHeight(3.0);
     cone->SetRadius(1.0);
-    cone->SetResolution(100); // smoother
+    cone->SetResolution(30);
 
     vtkNew<vtkPolyDataMapper> coneMapper;
     coneMapper->SetInputConnection(cone->GetOutputPort());
 
     vtkNew<vtkActor> coneActor;
     coneActor->SetMapper(coneMapper);
+    coneActor->GetProperty()->SetColor(1.0, 0.2, 0.2); // red
+    coneActor->SetPosition(-2, 0, 0); // move left
 
-    vtkNew<vtkRenderer> ren1;
-    ren1->AddActor(coneActor);
+    // --- Second object: Sphere ---
+    vtkNew<vtkSphereSource> sphere;
+    sphere->SetRadius(1.0);
+    sphere->SetThetaResolution(30);
+    sphere->SetPhiResolution(30);
 
+    vtkNew<vtkPolyDataMapper> sphereMapper;
+    sphereMapper->SetInputConnection(sphere->GetOutputPort());
 
-    vtkNew<vtkRenderWindow> renWin;
-    renWin->AddRenderer(ren1);
-    renWin->SetSize(600, 600);
-    renWin->SetWindowName("Interactive Cone");
+    vtkNew<vtkActor> sphereActor;
+    sphereActor->SetMapper(sphereMapper);
+    sphereActor->GetProperty()->SetColor(0.2, 0.6, 1.0); // blue
+    sphereActor->SetPosition(2, 0, 0); // move right
 
-    // Create interactor and link it to the render window
-    vtkNew<vtkRenderWindowInteractor> iren;
-    iren->SetRenderWindow(renWin);
+    // --- Renderer + Window + Interactor ---
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(coneActor);
+    renderer->AddActor(sphereActor);
+    renderer->SetBackground(0.2, 0.6, 0.4); // dark gray
 
-    vtkNew<vtkInteractorStyleTrackballCamera> style;
-    iren->SetInteractorStyle(style);
+    vtkNew<vtkLight> light;
+	light->SetLightTypeToSceneLight();
+	light->SetPosition(0, 5, 5);
+	light->SetFocalPoint(0, 0, 0);
+	light->SetColor(1, 1, 1);
+	light->SetIntensity(1);
 
+	renderer->AddLight(light);
 
-    // Render once before starting interactor
-    renWin->Render();
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetSize(800, 600);
 
-    // Start interaction (keeps window open)
-    iren->Start();
+    vtkNew<vtkRenderWindowInteractor> interactor;
+    interactor->SetRenderWindow(renderWindow);
 
-    return EXIT_SUCCESS;
+	vtkNew<vtkInteractorStyleTrackballCamera> style;
+	interactor->SetInteractorStyle(style);
+
+    // Render + Start interaction
+    renderWindow->Render();
+    interactor->Start();
+
+    return 0;
 }
